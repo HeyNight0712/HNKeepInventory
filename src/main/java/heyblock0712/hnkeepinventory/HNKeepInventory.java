8d1c0493e12d8599cd193cab07172e73339227cf
blacknight0712 <blacknight0981@gmail.com>
2024-02-07 14:54:27 +0800

    初始提交

---
+++ src/main/java/heyblock0712/hnkeepinventory/HNKeepInventory.java
@@ -0,0 +1,17 @@
+package heyblock0712.hnkeepinventory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HNKeepInventory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
